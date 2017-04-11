d4d469d54182bcda905651150b2f9c12b268a9a1
Bruno <ero-xd@hotmail.com>
2017-04-11 11:28:48 -0300

    foi

--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,21 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+let iterador = 0
+let proximo = document.getElementById('proximo');
+let anterior = document.getElementById('anterior');
+
+proximo.addEventListener('click', function(e){
+		if(iterador<todasAsImagens.lengtn()){
+			iterador++;
+			proximo.parentNode.firstChild.src = servidorDasImagens + todasAsImagens[iterador];
+		}
+	});
+
+anterior.addEventListener('click', function(e){
+		if(iterador>0){
+			iterador--;
+			anterior.parentNode.firstChild.src = servidorDasImagens + todasAsImagens[iterador];
+		}
+	});
